35b67bed3f3d977120c95ffd1f42745af5cffbe9
Brian  Marsh <Brian.Marsh@mastercard.com>
2016-09-19 14:22:17 -0500

    Added additional tests

--- Tests/Network-Helper.Tests.ps1
+++ Tests/Network-Helper.Tests.ps1
@@ -7,7 +7,7 @@ $ModuleName = "Network-Helper"
 $PSVersion = $PSVersionTable.PSVersion.Major
 Import-Module $PSScriptRoot\..\$ModuleName\$ModuleName.psm1 -Force
 
-#Integration Testing for SubnetMask conversion
+#Unit Testing for SubnetMask conversion
 Describe "ConvertTo-SubnetMask PS$PSVersion Integrations tests" {
 
     Context 'Strict mode' {
@@ -17,6 +17,25 @@ Describe "ConvertTo-SubnetMask PS$PSVersion Integrations tests" {
         It 'should return valid SubnetMask' {
             $Output = ConvertTo-SubnetMask -MaskLength 24
             $Output -eq "255.255.255.0" | Should be $True
+        }
+
+        It 'should error with a length SubnetMask over 32' {
+            try {
+                $Output = ConvertTo-SubnetMask -MaskLength 36 -ErrorAction Stop
+            } catch {
+                $error[0].exception -match "greater than the maximum allowed range of 32" `
+                        | Should be $True
+            }
+        }
+
+        It 'should error with a non-int subnetmask' {
+            try {
+                $Output = ConvertTo-SubnetMask -MaskLength "error" -ErrorAction Stop
+            } catch {
+                $error[0].exception -match "The argument cannot be validated because its type `"String`" is not the same type (Int32)*" `
+                        | Should be $True
+            }
+
         }
     }
 }
@@ -31,6 +50,16 @@ Describe "ConvertTo-MaskLength PS$PSVersion Integrations tests" {
         It 'should return valid mask length' {
             $Output = ConvertTo-MaskLength -SubnetMask '255.255.255.0'
             $Output -eq "24" | Should be $True
+        }
+
+        It 'should error when invalid IP is provided' {
+            try {
+                $Output = ConvertTo-MaskLength -SubnetMask '256.255.255.0' -ErrorAction Stop
+            } catch {
+                $Error[0].exception -match "Cannot convert .* to type `"System.Net.IPAddress`"" `
+                        | Should be $True
+            }
+
         }
     }
 }
